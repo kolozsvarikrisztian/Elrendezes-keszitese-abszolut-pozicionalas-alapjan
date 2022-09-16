eebede5961106ec0b2170d6bedae80b22d3dda95
kolozsvarikrisztian <laszlokrisztiankolozsvari@gmail.com>
2022-09-16 13:32:20 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Elrendezés_készítése_abszolút_pozícionálás_alapján
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
